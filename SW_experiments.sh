bb41031cb4c7859296ae874190a09d7ef4aab594
Ivan Lobov <i.lobov@criteo.com>
2018-02-09 21:00:49 +0100

    QIP

--- SW_experiments.sh
+++ SW_experiments.sh
@@ -1,5 +1,5 @@
 ### Timing experiments on already established solution with optimized SLEPc installation
-export COUNTS=/home/ivan/data/svd_testing/wiki_win=2_thr=100
+export COUNTS=/home/ivan/hyperwords/data/svd_testing/wiki_win=2_thr=100
 
 export OPTS="--pow 0.3 --dim 50 --max_iter 100 --tol 0.01"
 python -m hyperwords.utils.learn_spectral_embeddings $OPTS $COUNTS bethe_hessian
