f5646770629e388c86e1dea703c34ba73ef2b06a
Ivan Lobov <i.lobov@criteo.com>
2018-02-09 21:04:40 +0100

    QIP

--- SVD_experiments.sh
+++ SVD_experiments.sh
@@ -2,13 +2,13 @@
 export COUNTS=/home/ivan/hyperwords/data/svd_testing/wiki_win=2_thr=100
 
 export OPTS="--dim 50 --neg 1 --cds 0.75"
-python -m hyperwords.utils.counts2svd --dim 1 --neg 1 --cds 0.75 $COUNTS
+python -m hyperwords.utils.counts2svd $OPTS $COUNTS
 
 export OPTS="--dim 100 --neg 1 --cds 0.75"
-python -m hyperwords.utils.counts2svd --dim 1 --neg 1 --cds 0.75 $COUNTS
+python -m hyperwords.utils.counts2svd $OPTS $COUNTS
 
 export OPTS="--dim 300 --neg 1 --cds 0.75"
-python -m hyperwords.utils.counts2svd --dim 1 --neg 1 --cds 0.75 $COUNTS
+python -m hyperwords.utils.counts2svd $OPTS $COUNTS
 
 export OPTS="--dim 500 --neg 1 --cds 0.75"
-python -m hyperwords.utils.counts2svd --dim 1 --neg 1 --cds 0.75 $COUNTS
+python -m hyperwords.utils.counts2svd $OPTS $COUNTS
