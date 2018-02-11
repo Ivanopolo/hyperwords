71c9d90d8376cbc4cac147b13355199add0c699a
Ivan Lobov <i.lobov@criteo.com>
2018-02-11 13:06:15 +0100

    WIP

--- SVD_experiments.sh
+++ SVD_experiments.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 ### Timing experiments on SVD with different dimensions
-export COUNTS=/home/ivan/hyperwords/data/svd_testing/wiki_win=2_thr=100
-
-export OPTS="--dim 500 --neg 1 --cds 0.75"
-python -m hyperwords.utils.counts2svd $OPTS $COUNTS
+export COUNTS=/home/ivan/hyperwords/data/svd_testing/wiki_win=2_thr=30
 
 export OPTS="--dim 100 --neg 1 --cds 0.75"
 python -m hyperwords.utils.counts2svd $OPTS $COUNTS
+
+export OPTS="--pow 0.3 --dim 100 --max_iter 100 --tol 0.01"
+python -m hyperwords.utils.learn_spectral_embeddings $OPTS $COUNTS bethe_hessian
+
+export COUNTS=/home/ivan/hyperwords/data/svd_testing/wiki_win=2_thr=3
+
+export OPTS="--dim 100 --neg 1 --cds 0.75"
+python -m hyperwords.utils.counts2svd $OPTS $COUNTS
+
+export OPTS="--pow 0.3 --dim 100 --max_iter 100 --tol 0.01"
+python -m hyperwords.utils.learn_spectral_embeddings $OPTS $COUNTS bethe_hessian
+
